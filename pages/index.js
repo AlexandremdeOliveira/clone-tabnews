e7c926e8d97ddd680f2a9641eab465f7c1eb75d8
AlexandremdeOliveira <75919345+AlexandremdeOliveira@users.noreply.github.com>
2025-10-21 09:16:36 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Todos deveriam aprender a programar um computador, pois isto ensina a pensar" - Steve Jobs</h1>
+    return <h1>Todos deveriam aprender a programar um computador, pois isto ensina a pensar." - Steve Jobs</h1>
 }
 
 export default Home;
